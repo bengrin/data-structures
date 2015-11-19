aaa5208e89cb8fe9414d5d1c9c25d404bb83fa0e
Ben Grinberg <bengrin25@gmail.com>
2015-11-19 14:03:27 -0500

    Completed prototypal queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  obj.numQueues = 0;
+  obj.place = 0;
+  obj.lastCalled = 0;
+
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.place] = value;
+    this.numQueues++;
+    this.place++;
+  },
+  dequeue: function() {
+    if (this.numQueues > 0 ) {
+      this.numQueues--;
+      var result = this.storage[this.lastCalled];
+      delete this.storage[this.lastCalled];
+      this.lastCalled++;
+      return result;
+    }
+  },
+  size: function() {
+    return this.numQueues;
+  }
+};
 
 
