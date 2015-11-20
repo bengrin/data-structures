9199017b7cfea2db5f143633c2eca07bb77dfce3
Ben Grinberg <bengrin25@gmail.com>
2015-11-19 20:02:01 -0500

    Completed linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (this.head === null) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      this.tail.next = new Node(value);
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var old = this.head;
+    this.head = this.head.next;
+    return old.value;
   };
 
   list.contains = function(target){
+    var currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } 
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +44,10 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * LinkedList - Constant
+ * addToTail - Constant
+ * removeHead - Constant
+ * contains - Linear
+ * Node - Constant
  */
+
